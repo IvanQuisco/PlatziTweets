7a244feb81346404a0508f4a4ecccd4a4cf874bb
ivanquisco <ivanquisco@gmail.com>
2020-06-01 13:08:24 -0500

    first commit, gitignore

---
+++ PlatziTweets/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PlatziTweets
+//
+//  Created by Ivan Quintana on 01/06/20.
+//  Copyright © 2020 Ivan Quintana. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
